577e5f3df761d78340474519ec75278bc4ec8396
backjello <jackbello97@gmail.com>
2024-02-29 23:20:32 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+let htmlPageNames = ['home'];
+const entry = {}
+let multipleHtmlPlugins = htmlPageNames.map(name => {
+    entry[name] = `./src/pages/${name}/${name}.ts`
+    return new HtmlWebpackPlugin({
+        template: `./src/pages/${name}/${name}.html`, // relative path to the HTML files
+        inject: true,
+        filename: `${name}.html`, // output HTML files
+        chunks: [`${name}`] // respective JS files
+    })
+});
+
+module.exports = {
+    entry: entry,
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+
+    plugins: multipleHtmlPlugins,
+
+    devServer: {
+        static: path.join(__dirname, "dist"),
+        compress: true,
+        port: 4000,
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        },
+    },
+};
