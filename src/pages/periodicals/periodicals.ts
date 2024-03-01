370ebefc6d39a43f1d11919266e042e65ea0bbdb
backjello <jackbello97@gmail.com>
2024-03-01 16:33:36 +0100

    16.30

--- src/pages/periodicals/periodicals.ts
+++ src/pages/periodicals/periodicals.ts
@@ -5,8 +5,21 @@ async function main() {
     const api = new Apiservice()
     const periodicals = await api.get('periodicals')
 
-    console.log(periodicals);
+    let html = ''
+    periodicals.data.data.forEach((periodical: any) => {
+        html += `<div>
+            <h2>${periodical.title}</h2>
+            <span>${periodical.type}</span> - 
+            <span>prezzo: ${periodical.price}€</span>
+        </div>`
 
+        periodical.categories.forEach((cat: any) => {
+            html += `
+                <span>${cat.name}</span>
+            `
+        })
+    });
+    document.getElementById('periodicals')!.innerHTML = html
 }
 
 main()
